97c3a8f76a6e7ec304132b0ca22d77478d922984
Viktor Førrisdahl <viktorkirkeby15@gmail.com>
2026-07-11 10:50:44 +0200

    Made for loop in initBoard to have fewer lines of code. Same with initBoardGrid. Made it more clean.

--- src/board.cpp
+++ src/board.cpp
@@ -181,86 +181,66 @@ void board::placeCross(int index)
 
 void board::initBoard()
 {
+    //make all sqaures white:
+    for (auto i = 0; i < 4; i++)
+    {
+        this->game_lines_[i].setFillColor(sf::Color::White);
+    }
     //Left vertical line
     this->game_lines_[0].setOrigin(sf::Vector2f(25.f,0.f));
     this->game_lines_[0].setPosition(400.f,0.f);
     this->game_lines_[0].setSize(sf::Vector2f(50.f,800.f));
-    this->game_lines_[0].setFillColor(sf::Color::White);
 
     //Right vertical line
     this->game_lines_[1].setOrigin(sf::Vector2f(25.f,0.f));
     this->game_lines_[1].setPosition(800.f,0.f);
     this->game_lines_[1].setSize(sf::Vector2f(50.f,800.f));
-    this->game_lines_[1].setFillColor(sf::Color::White);
 
     //Upper horizontal line
     this->game_lines_[2].setOrigin(sf::Vector2f(0.f,25.f));
     this->game_lines_[2].setPosition(0.f,266.f);
     this->game_lines_[2].setSize(sf::Vector2f(1200.f,50.f));
-    this->game_lines_[2].setFillColor(sf::Color::White);
 
     //Lower horizontal line
     this->game_lines_[3].setOrigin(sf::Vector2f(0.f,25.f));
     this->game_lines_[3].setPosition(0.f,532.f);
     this->game_lines_[3].setSize(sf::Vector2f(1200.f,50.f));
-    this->game_lines_[3].setFillColor(sf::Color::White);
 }
 
 void board::initBoardGrid() {
+    for (auto i = 0; i < 9; i++)
+    {
+        this->grid_[i].setOrigin(sf::Vector2f(175.f,0.f));
+        this->grid_[i].setSize(sf::Vector2f(350.f,215.f));
+        this->grid_[i].setFillColor(sf::Color::Black);
+    }
+
     //top left gridbox
-    this->grid_[0].setOrigin(sf::Vector2f(175.f,0.f));
     this->grid_[0].setPosition(187.5,10.f);
-    this->grid_[0].setSize(sf::Vector2f(350.f,215.f));
-    this->grid_[0].setFillColor(sf::Color::Black);
 
     //top middle gridbox
-    this->grid_[1].setOrigin(sf::Vector2f(175.f,0.f));
     this->grid_[1].setPosition(600.f,10.f);
-    this->grid_[1].setSize(sf::Vector2f(350.f,215.f));
-    this->grid_[1].setFillColor(sf::Color::Black);
 
     //top right gridbox
-    this->grid_[2].setOrigin(sf::Vector2f(175.f,0.f));
     this->grid_[2].setPosition(1012.5,10.f);
-    this->grid_[2].setSize(sf::Vector2f(350.f,215.f));
-    this->grid_[2].setFillColor(sf::Color::Black);
 
     //middle left gridbox
-    this->grid_[3].setOrigin(sf::Vector2f(175.f,0.f));
     this->grid_[3].setPosition(187.5,292.f);
-    this->grid_[3].setSize(sf::Vector2f(350.f,215.f));
-    this->grid_[3].setFillColor(sf::Color::Black);
 
     //middle middle gridbox
-    this->grid_[4].setOrigin(sf::Vector2f(175.f,0.f));
     this->grid_[4].setPosition(600.f,292.f);
-    this->grid_[4].setSize(sf::Vector2f(350.f,215.f));
-    this->grid_[4].setFillColor(sf::Color::Black);
 
     //middle right gridbox
-    this->grid_[5].setOrigin(sf::Vector2f(175.f,0.f));
     this->grid_[5].setPosition(1012.5,292.f);
-    this->grid_[5].setSize(sf::Vector2f(350.f,215.f));
-    this->grid_[5].setFillColor(sf::Color::Black);
 
     //bottom left gridbox
-    this->grid_[6].setOrigin(sf::Vector2f(175.f,0.f));
     this->grid_[6].setPosition(187.5,572.5);
-    this->grid_[6].setSize(sf::Vector2f(350.f,215.f));
-    this->grid_[6].setFillColor(sf::Color::Black);
 
     //bottom middle gridbox
-    this->grid_[7].setOrigin(sf::Vector2f(175.f,0.f));
     this->grid_[7].setPosition(600.f,572.5);
-    this->grid_[7].setSize(sf::Vector2f(350.f,215.f));
-    this->grid_[7].setFillColor(sf::Color::Black);
 
     //bottom right gridbox
-    this->grid_[8].setOrigin(sf::Vector2f(175.f,0.f));
     this->grid_[8].setPosition(1012.5,572.5);
-    this->grid_[8].setSize(sf::Vector2f(350.f,215.f));
-    this->grid_[8].setFillColor(sf::Color::Black);
-
 }
 
 void board::initEmptyCircles()
